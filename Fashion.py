13afe53d3815382e2fe1ff65f9d622c0076d4cb2
rcthota <varshan004@gmail.com>
2019-04-11 12:54:40 +1000

    Initial commit

---
+++ Fashion.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+fashion_mnist=keras.datasets.fashion_mnist
+
+(train_images,train_labels),(test_images,test_labels)=fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
